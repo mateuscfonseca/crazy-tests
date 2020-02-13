f41c1bf3f59bc14bb3dae6f17a62af6ac7c91f39
mateuscfonseca <mateuscfonseca@gmail.com>
2020-02-13 08:05:27 -0300

    CatAndMouse, MagicSquare

--- src/hackerhank/SockMerchant.java
+++ src/hackerhank/SockMerchant.java
@@ -9,9 +9,9 @@ public class SockMerchant {
 	static int sockMerchant(int n, int[] ar) {
 		Map<Integer, Integer> map = new HashMap<>();
 		IntStream
-			.range(0, ar.length)
+			.range(0, n)
 			.forEach(i -> {
-				map.compute(i, (k, v) -> v == null ? 1 : v + 1);
+				map.compute(ar[i], (k, v) -> v == null ? 1 : v + 1);
 			});
 		return map.values().stream().map(v -> v / 2).reduce((acc, v) -> acc + v).get();
     }
